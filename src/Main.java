c27be7cd8f3acf0e355082b2ae1cab14697a6b6b
KrAjay62604 <ak62604@gmail.com>
2025-08-11 19:14:04 +0530

    super keyword in action.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Hero hero1= new Hero("Spiderman", 42, "Spider web");
+        System.out.println(hero1.name + " "+ hero1.age+ " "+ hero1.power);
+    }
+}
